f7e7570ce0e81ee6859d770f188767141a73dfbc
Muhammadali <rahimov.dev1@gmail.com>
2025-07-14 13:21:06 +0500

    second commit

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,8 +1,8 @@
+// src/api/Api.js
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://localhost:5000/'
-})
+    baseURL: "https://sohil-backend-1.onrender.com/"
+});
 
-
-export default Api
+export default Api;
